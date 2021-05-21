309a0a48ddb2030d8e34ade3af0395634e2dcf8d
victoria0406 <81007362+victoria0406@users.noreply.github.com>
2021-05-22 05:33:23 +0900

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import './style/App.css';
 import challengesend from './components/challenge-send';
 import management from './components/management';
@@ -6,11 +5,15 @@ import challenge from './components/challenge';
 import mileage from './components/mileage';
 import win from './components/win';
 import check from './components/check';
+import Login from './components/login';
+import Group from './components/group';
+//import Mapping from './components/Map';
 
 import {
   BrowserRouter as Router,
   Route,
 } from "react-router-dom";
+import { Map } from 'google-maps-react';
 
 
 function App() {
@@ -23,6 +26,8 @@ function App() {
         <Route path="/mileage" exact component={mileage}/>
         <Route path="/win" exact component={win}/>
         <Route path="/check" exact component={check}/>
+        <Route path="/login" exact component ={Login}/>
+        <Route path="/group/:id" exact component = {Group}/>
       </switch>
     </Router>
   );
