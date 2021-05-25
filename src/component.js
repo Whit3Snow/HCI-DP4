d4ed42c9e6d1e3802ea49d766daf66ca780e3ea1
Hyeongjune-Park <80369134+Hyeongjune-Park@users.noreply.github.com>
2021-05-25 21:51:40 +0900

    Add files via upload

--- src/component.js
+++ src/component.js
@@ -1,17 +1,19 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import './component.css';
-import ReactDOM from 'react-dom';
+import ReactDOM, {render} from 'react-dom';
 import jQuery from "jquery";
-import { render } from 'react-dom/cjs/react-dom.development';
+import {storage} from "./firebase";
 import axios from 'axios';
+
 
 
 window.$ = window.jQuery = jQuery;  
 
+var isEmpty = 0;
 var x;
 var y;
-
-
+var url1;
+var html_2;
 
 class Menu extends React.Component{
     render(){
@@ -42,41 +44,56 @@ function off(){
 }
 
 
-class component extends Component{
 
-    constructor(props) {
-        super(props);
-        this.state = {
-          selectedFile: null,
+function Component(){
+        const [image, setImage] = useState(null)
+        const [url, setUrl] = useState("");
+        
+        const[progress, setProgress] = useState(0);
+        const handleChange = e => {
+            if (e.target.files[0]) {
+                setImage(e.target.files[0]);
+                }
         }
-      }
-    
-      handleFileInput(e){
-        this.setState({
-          selectedFile : e.target.files[0],
-          
-        })
-        console.log(e.target.files[0]);
-      }
-    
-      handlePost(){
-        const formData = new FormData();
-        formData.append('file', this.state.selectedFile);
-        return axios.post("/api/upload", formData).then(res => {
-          alert('성공')
-        }).catch(err => {
-          alert('실패')
-        })
-    }
-    render() {
+        const handleUpload = () => {
+            const uploadTask = storage.ref(`images/${image.name}`).put(image);
+            uploadTask.on(
+              "state_changed",
+              snapshot => {
+                const progress = Math.round(
+                  (snapshot.bytesTransferred/ snapshot.totalBytes) * 100
+                );
+                setProgress(progress);
+              },
+              error => {
+                console.log(error);
+              },
+              () => {
+                storage
+                  .ref("images")
+                  .child(image.name)
+                  .getDownloadURL()
+                  .then(url => {
+                    setUrl(url);
+                  });
+                }
+                
+            )
+            
+            isEmpty++;
+            
+        };
+
+        url1 = url;
         return(
         <body>
             <div class = "bar"></div>
             <input type = "text" class = "setTitle" placeholder = " Title"/>
             <input type = "text" class = "setTag"/>
             <div class = "upload">
-                <input type="file" name="file" class = "file" onChange={e => this.handleFileInput(e)}/>
-                <button type="button" class = "upbutton" onClick={()=>this.handlePost()}>Upload</button>
+                <input type="file" class = "file" onChange={handleChange}/>
+                <progress value = {progress} max = "100" class = "progress"/>
+                <button type="button" class = "upbutton" onClick={handleUpload}>Upload</button>
             </div>
             <button class = "Tag">Tag Register</button>
             <div class = "setCal"></div>
@@ -102,7 +119,8 @@ class component extends Component{
             </table>
             <div class = "Paper">
                 <div id = "component1" class = "component1"></div>
-                <div id = "component2" class = "component2"></div>
+                
+                <table id = "component2" class = "component2"></table>
                 <div class = "bone"></div>
             </div>
             
@@ -121,18 +139,33 @@ class component extends Component{
             </div>
         </body>
     )
-    }
+    
 }
 
+function Uploading(){
+    if(!isEmpty){
+        alert("No Image Upload first");
+    }
+    else{
+        const comp = (
+            <img src = {url1} alt = "firebase-image" class = "image1"/>
+            );
+        ReactDOM.render(comp, document.getElementById('putimg'));
+    }
+
+}
 
 function icon1(){
     const comp = (
         <div id = "stop">
             <div class = "draggable" id = "draggable">C{"\n"}A{"\n"}T{"\n"}C{"\n"}H</div>
             <div class = "delete1" id = "delete1" onClick = {()=>delete1()}>D E L E T E</div>
-            <div class = "scoresheet" >
-                <div class = "putimg">
-
+            <div class = "scoresheet">
+                <div class = "putimg" id = "putimg">
+                    <div class = "addimg" onClick = {()=>Uploading()} >+</div>
+                    <table class = "uploadlist">
+                        <tr><td>Empty</td></tr>
+                    </table>
                 </div>
                 <table class = "set" >
                     <tr>
@@ -164,17 +197,16 @@ function icon1(){
 }
 
 function icon2(){
-    const comp = (
-        <table class = "score">
-            <tr class = "draggable">
-                <td >set</td>
-            </tr>
-            <tr>
-                <td>set</td>
-            </tr>
-        </table>
-    );
-    ReactDOM.render(comp, document.getElementById('component2'));
+    const comp = 
+        '<div>'+
+            '<div class = "a1"></div>'+
+            '<textarea id = "textbox"></textarea>'+
+        '</div>';
+
+    
+
+    const newcomp = <div dangerouslySetInnerHTML={ {__html: comp+html_2} }></div>
+    ReactDOM.render(newcomp, document.getElementById('component1'));
 }
 
 function delete1(){
@@ -183,6 +215,10 @@ function delete1(){
     )
     ReactDOM.render(comp, document.getElementById('component1'));
 }
+
+
+//---------------------------------------------drag on-----------------------------------------------------
+//---------------------------------------------------------------------------------------------------------
 
 let isDragging = false;
 
@@ -293,4 +329,112 @@ document.addEventListener('mousedown', function(event) {
 
 });
 
-export default component;
+
+document.addEventListener('mousedown', function(event) {
+
+  let dragElem = event.target.closest('.draggable2');
+
+  if (!dragElem) return;
+
+  let dragElement = jQuery("#draggable2").parents("div")[0]
+
+  event.preventDefault();
+
+  dragElement.ondragstart = function() {
+      return false;
+  };
+
+  let coords, shiftX, shiftY;
+
+  startDrag(dragElement, event.clientX, event.clientY);
+
+  function onMouseUp(event) {
+    finishDrag();
+  };
+
+  function onMouseMove(event) {
+    moveAt(event.clientX, event.clientY);
+  }
+
+  function startDrag(element, clientX, clientY) {
+    
+    if(isDragging) {
+      return;
+    }
+    
+    isDragging = true;
+
+    document.addEventListener('mousemove', onMouseMove);
+    element.addEventListener('mouseup', onMouseUp);
+
+    shiftX = clientX - element.getBoundingClientRect().left;
+    shiftY = clientY - element.getBoundingClientRect().top;
+
+
+    element.style.position = 'fixed';
+
+    moveAt(clientX, clientY);
+  };
+
+  function finishDrag() {
+    if(!isDragging) {
+      return;
+    }
+
+    isDragging = false;
+
+    dragElement.style.top = parseInt(dragElement.style.top) + window.pageYOffset + 'px';
+    dragElement.style.position = 'absolute';
+    
+    x = dragElement.style.left;
+    y = dragElement.style.top;
+
+    console.log(dragElement.id, x, y);
+
+    document.removeEventListener('mousemove', onMouseMove);
+    dragElement.removeEventListener('mouseup', onMouseUp);
+  }
+
+  function moveAt(clientX, clientY) {
+      
+    let newX = clientX - shiftX;
+    let newY = clientY - shiftY;
+
+    let newBottom = newY + dragElement.offsetHeight;
+
+    if (newBottom > document.documentElement.clientHeight) {
+
+      let docBottom = document.documentElement.getBoundingClientRect().bottom;
+
+
+      let scrollY = Math.min(docBottom - newBottom, 10);
+
+      if (scrollY < 0) scrollY = 0;
+
+      window.scrollBy(0, scrollY);
+
+      newY = Math.min(newY, document.documentElement.clientHeight - dragElement.offsetHeight);
+    }
+
+    if (newY < 0) {
+
+      let scrollY = Math.min(-newY, 10);
+      if (scrollY < 0) scrollY = 0;
+
+      window.scrollBy(0, -scrollY);
+
+    }
+
+    if (newX < 0) newX = 0;
+    if (newX > document.documentElement.clientWidth - dragElement.offsetWidth) {
+      newX = document.documentElement.clientWidth - dragElement.offsetWidth;
+    }
+
+    dragElement.style.left = newX + 'px';
+    dragElement.style.top = newY + 'px';
+    
+  }
+
+});
+
+export default Component;
